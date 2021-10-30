437346ff3365aab915292980876bf3ce6cf07d02
Jordan Deck <jordan@bankworx.com>
2021-10-30 18:30:51 -0500

    📦  FEAT: [pokemon model] add pokemon model and update root store with data

---
+++ app/models/pokemon/pokemon.ts
@@ -0,0 +1,22 @@
+import { Instance, SnapshotOut, types } from "mobx-state-tree"
+
+/**
+ * Model description here for TypeScript hints.
+ */
+export const PokemonModel = types
+  .model("Pokemon")
+  .props({
+    id: types.identifier,
+    name: types.string,
+    types: types.array(types.string),
+    sprite: types.string,
+    base_experience: types.number,
+  })
+  .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .actions((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export type PokemonType = Instance<typeof PokemonModel>
+export interface Pokemon extends PokemonType {}
+type PokemonSnapshotType = SnapshotOut<typeof PokemonModel>
+export interface PokemonSnapshot extends PokemonSnapshotType {}
+export const createPokemonDefaultModel = () => types.optional(PokemonModel, {})
